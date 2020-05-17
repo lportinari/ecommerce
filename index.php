b99e7e06e648740a360e16985eba130a89ba90fa
Luciano Celinski <luciano@gruposc.com.br>
2020-05-17 13:57:06 -0300

    Sistema de login

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -19,10 +20,46 @@ $app->get('/', function() {
 });
 
 $app->get('/admin', function() {
+
+	//Valida se o usuário está logado
+	User::verifyLogin();
     
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+$app->get('/admin/login', function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl('login');
+
+});
+
+//Rota para o método post para a página de login
+$app->post('/admin/login', function(){
+
+	//Validação do usuário na classe User
+	User::login($_POST["login"], $_POST["password"]);
+
+	//Redirecionando para a homepage da administração
+	header("Location: /admin");
+	exit;
+
+});
+
+//Rota para o Logout da sessão de usuário
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
